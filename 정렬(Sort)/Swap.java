7d0096082ad231bdc8ddc18247f9626510f23bf5
UmJaeJeong <39511137+UmJaeJeong@users.noreply.github.com>
2022-04-29 11:33:32 +0900

    스와프
    스와프 예제 코드

---
+++ 정렬(Sort)/Swap.java
@@ -0,0 +1,16 @@
+package Sort;
+
+public class Swap {
+
+    public static void main(String[] args) {
+
+        int[] arr = {3,5};
+
+        //스와프(Swap)
+        int tmep = arr[0];
+        arr[0] = arr[1];
+        arr[1] = temp;
+
+        System.out.println(arr[0]+" "+arr[1]);
+    }
+}
