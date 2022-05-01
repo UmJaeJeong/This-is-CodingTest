df14807f1f4849b8509f74eb3b42c24982fc4f48
UmJaeJeong <39511137+UmJaeJeong@users.noreply.github.com>
2022-05-01 22:58:19 +0900

    삽입정렬
    삽입정렬

---
+++ 정렬(Sort)/InsertionSort.java
@@ -0,0 +1,26 @@
+package Sort;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+
+        int n = 10;
+        int[] arr = {7,5,9,0,3,1,6,2,4,8};
+
+        for(int i=1; i<n; i++){
+            //인덱스 i부터 1까지 감소하며 반복하는 문법
+            for(int j=i; j>0;j--){
+                //한칸씩 왼쪽으로 이동
+                if(arr[j]<arr[j-1]){
+                    //스와프(swap)
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1]= temp;
+                }
+                //자기보다 작은 데이터를 만나면 그 위치에서 멈춤
+                else break;
+            }
+        }
+
+    }
+}
