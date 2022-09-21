9d068f75e30c05a9ff2d1e349d968f44e91e1783
UmJaeJeong <39511137+UmJaeJeong@users.noreply.github.com>
2022-09-21 23:05:17 +0900

    프로그래머스
    코딩테스트 연습
    월간 코드 챌린지 시즌2
    음양 더하기

---
+++ 프로그래머스복습/음양 더하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        int size = absolutes.length;
+        for(int i=0; i<size;i++){
+            if(signs[i]){
+                answer+=absolutes[i];
+
+            }else{
+                answer+=absolutes[i]*-1;
+            }
+        }
+        return answer;
+    }
+}
