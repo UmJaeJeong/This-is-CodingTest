d4abb0e5a5421cddc4f2db57566dddac2318db87
UmJaeJeong <39511137+UmJaeJeong@users.noreply.github.com>
2022-10-26 20:59:14 +0900

    네트워크
    코딩테스트 연습
    깊이/너비 우선 탐색(DFS/BFS)
    네트워크

---
+++ 프로그래머스복습/네트워크1.java
@@ -0,0 +1,31 @@
+//다시풀기
+import java.util.*;
+
+class Solution {
+    boolean[] visited;
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new boolean[n];
+        
+        for(int i=0; i<n;i++){
+            if(!visited[i]){
+                dfs(i,computers,n);
+                answer++;
+            }
+            
+        }
+        
+        return answer;
+    }
+    
+    public void dfs(int idx, int[][] computers, int n){
+        //탈출
+        
+        //계산
+        visited[idx] = true;
+        for(int i=0; i<n;i++){
+            if(!visited[i] && computers[idx][i]==1) dfs(i,computers,n);
+        }
+    }
+ 
+}
