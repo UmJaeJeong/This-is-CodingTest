4c7d8ff7b1eb1df0b25075023074cac5133d95a6
UmJaeJeong <39511137+UmJaeJeong@users.noreply.github.com>
2022-11-12 23:44:52 +0900

    전력망을 둘로 나누기
    코딩테스트 연습
    완전탐색
    전력망을 둘로 나누기

---
+++ 프로그래머스복습/전력망을 둘로 나누기4.java
@@ -0,0 +1,56 @@
+//다시풀기
+
+import java.util.*;
+
+class Solution {
+    
+    ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+    public int solution(int n, int[][] wires) {
+        int answer = Integer.MAX_VALUE;
+        
+        //ArrayList 전력망 대입
+        for(int i=0; i<=n;i++){
+            graph.add(new ArrayList<Integer>());
+        }
+        
+        for(int i=0; i<wires.length;i++){
+            int a = wires[i][0];
+            int b = wires[i][1];
+            
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+
+        }
+       
+        //node 1개를 선택하여 이어져 있는 노드를 끊는다.
+        for(int i=0; i<wires.length;i++){
+           int a = bfs(wires[i][0],wires[i][1],n);
+           int b = bfs(wires[i][1],wires[i][0],n);
+            answer = Math.min(answer,Math.abs(a-b));
+        }
+            //끊어진 상태의BFS 탐색
+        return answer;
+    }
+    
+    public int bfs(int x, int y, int n){
+        boolean[] visited = new boolean[n+1];
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(x);
+        visited[x] = true;
+        visited[y] = true;
+        int sum = 1;
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            
+            for(int next : graph.get(cur)){
+                if(!visited[next]){
+                    sum+=1;
+                    visited[next] =  true;
+                    q.add(next);
+                }
+            }
+        }
+        return sum;
+    }
+}
